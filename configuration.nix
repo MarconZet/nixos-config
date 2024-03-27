75505701fea2608c1e3fd12924f2434deeeb32c9
MarconZet <25779550+MarconZet@users.noreply.github.com>
2024-03-27 13:21:22 +0100

    34 current  2024-03-27 13:21:19  24.05.20240323.44d0940   6.6.22                          *

--- configuration.nix
+++ configuration.nix
@@ -110,22 +110,22 @@
   virtualisation.vmware.guest.enable = true;
   networking.bridges = {mylxdbr0.interfaces = [];};
 
-  #networking.localCommands = ''
-  #  ip address add 192.168.57.1/24 dev mylxdbr0
-  #'';
+  networking.localCommands = ''
+    ip address add 192.168.57.1/24 dev mylxdbr0
+  '';
 
-  #networking.firewall.extraCommands = ''
-  #  iptables -A INPUT -i mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
 
-  #  # These three technically aren't needed, since by default the FORWARD and
-  #  # OUTPUT firewalls accept everything everything, but lets keep them in just
-  #  # in case.
-  #  iptables -A FORWARD -o mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
-  #  iptables -A FORWARD -i mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
-  #  iptables -A OUTPUT -o mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
+    # These three technically aren't needed, since by default the FORWARD and
+    # OUTPUT firewalls accept everything everything, but lets keep them in just
+    # in case.
+    iptables -A FORWARD -o mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
+    iptables -A FORWARD -i mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
+    iptables -A OUTPUT -o mylxdbr0 -m comment --comment "my rule for LXD network mylxdbr0" -j ACCEPT
 
-  #  iptables -t nat -A POSTROUTING -s 192.168.57.0/24 ! -d 192.168.57.0/24 -m comment --comment "my rule for LXD network mylxdbr0" -j MASQUERADE
-  #'';
+    iptables -t nat -A POSTROUTING -s 192.168.57.0/24 ! -d 192.168.57.0/24 -m comment --comment "my rule for LXD network mylxdbr0" -j MASQUERADE
+  '';
 
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = true;
