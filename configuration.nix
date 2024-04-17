ef5336a2a72ba1b7311c56bf3ebef59b01d50787
MarconZet <25779550+MarconZet@users.noreply.github.com>
2024-04-17 23:34:55 +0200

    50 current  2024-04-17 23:34:49  24.05.20240329.d8fe5e6   6.6.23                          *

--- configuration.nix
+++ configuration.nix
@@ -153,7 +153,7 @@
   #   enableSSHSupport = true;
   # };
 
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
