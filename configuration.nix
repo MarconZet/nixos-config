d79d69b5c5a77622b4fe976a5e5511d0260940a0
MarconZet <25779550+MarconZet@users.noreply.github.com>
2024-07-11 01:45:35 +0200

    19 current  2024-07-11 01:45:33  24.11.20240708.655a58a   6.6.37                          *

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # Set your time zone.
