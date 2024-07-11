da195541fd1a495580a6f1ceea155c2055b08452
MarconZet <25779550+MarconZet@users.noreply.github.com>
2024-07-12 00:09:05 +0200

    26 current  2024-07-12 00:09:02  24.11.20240708.655a58a   6.6.37                          *

--- configuration.nix
+++ configuration.nix
@@ -213,7 +213,7 @@
   # };
 
   services.openssh.enable = true;
-
+  services.openssh.settings.PasswordAuthentication = false;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
