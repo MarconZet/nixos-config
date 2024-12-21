4fd75c7a5dc82d1621364a7d45f52efca2b37063
MarconZet <25779550+MarconZet@users.noreply.github.com>
2024-12-21 03:57:48 +0100

    189 current  2024-12-21 03:57:35  25.05.20241205.d0797a0   6.6.63                          *

--- home/ui/default.nix
+++ home/ui/default.nix
@@ -17,6 +17,27 @@
     extraConfig = builtins.readFile ./hypr/hyprlock.conf;
   };
 
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 600;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
+
   programs.waybar = {
     enable = true;
     systemd.enable = false;
